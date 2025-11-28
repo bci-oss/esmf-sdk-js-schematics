77839c926f5df83ad0a6ae334a3d13d6a89f96fc
Pavel Shalamkov <hpl7fe@bosch.com>
2025-11-28 17:08:16 +0400

    Add config-menu.component.ts
    Signed-off-by: Pavel Shalamkov <hpl7fe@bosch.com>

---
+++ libs/schematic/src/lib/services/storage.service.ts
@@ -0,0 +1,9 @@
+import {Injectable} from '@angular/core';
+
+// TODO: It's a temporary stub for the JSSdkLocalStorageService. Will be removed during the actual migration of the service.
+@Injectable()
+export class JSSdkLocalStorageService {
+  setItem(x, y) {
+    console.log(x, y);
+  }
+}
